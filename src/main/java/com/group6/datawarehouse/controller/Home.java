c767bae735137e2b1cb0aecb6a7b0fc960b1fdc7
ttdongnlu <20130230@st.hcmuaf.edu.vn>
2023-12-12 12:35:37 +0700

    12/12/2023

--- src/main/java/com/group6/datawarehouse/controller/Home.java
+++ src/main/java/com/group6/datawarehouse/controller/Home.java
@@ -1,7 +1,6 @@
 package com.group6.datawarehouse.controller;
 
 import com.group6.datawarehouse.dao.DBDM;
-import com.group6.datawarehouse.model.CurrencyModel;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -9,6 +8,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -16,35 +16,59 @@ import java.util.List;
 
 import com.group6.datawarehouse.model.Currency;
 
-@WebServlet("/trang-chu")
+@WebServlet(name = "trang-chu", value = "/trang-chu")
 public class Home extends HttpServlet {
     private DBDM dbdm = new DBDM();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        resp.setCharacterEncoding("UTF-8");
-        req.setCharacterEncoding("UTF-8");
-
-        String bank = req.getParameter("bank");
-
+        String path = req.getServletContext().getRealPath("/") + File.separator + "tool" + File.separator + "HomeEx.java";
+        System.out.println("path: " + path);
+        System.out.println("doget");
+//        resp.sendRedirect("view/Trangchu.jsp");
         Currency currency = new Currency();
         List<Currency> arrayListTienTe = new ArrayList<>();
         try {
-            arrayListTienTe = dbdm.loadCurrency();
+            arrayListTienTe = dbdm.loadCurrency("acb", "2023-12-10");
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        } finally {
+            dbdm.getiConnect().closeConnection();
         }
-        dbdm.getiConnect().closeConnection();
-
         req.setAttribute("arrayListTienTe", arrayListTienTe);
         RequestDispatcher rd = req.getRequestDispatcher("view/Trangchu.jsp");
         rd.forward(req, resp);
-//        resp.sendRedirect("view/Trangchu.jsp");
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-    }
+
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        System.out.println("doPossr");
+//        resp.setContentType("text/html");
+//        resp.setCharacterEncoding("UTF-8");
+//
+//        String selectedBank = req.getParameter("bank");
+//        String selectedDate = req.getParameter("date");
+//
+//        System.out.println(selectedBank);
+//        System.out.println(selectedDate);
+//        if (selectedBank.isEmpty() || selectedDate.isEmpty()) {
+//           doGet(req,resp);
+//            System.out.println("emty");
+//        } else {
+//            Currency currency = new Currency();
+//            List<Currency> arrayListTienTe = new ArrayList<>();
+//            try {
+//                arrayListTienTe = dbdm.loadCurrency(selectedBank,selectedDate);
+//            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+//            }finally {
+//                dbdm.getiConnect().closeConnection();
+//            }
+//            req.setAttribute("arrayListTienTe", arrayListTienTe);
+//            RequestDispatcher rd = req.getRequestDispatcher("view/Trangchu.jsp");
+//            rd.forward(req, resp);
+//        }
+//
+//    }
 }
